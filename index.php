3e5ee3ca59207241624ae7e7e6fe891f043fc3e3
qwqtw <qiutianw@gmail.com>
2024-07-16 12:48:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,25 @@
 <?php
 // Load the composer required libraries
 require "vendor/autoload.php";
-
 // Load the framework
 $f3 = Base::instance();
-
-// Automatically load the controller classes and views
-$f3->set("AUTOLOAD", "controllers/");
-$f3->set("UI", "views/");
-
-// Enable debug
-// TODO: Remove for production
-$f3->set("DEBUG", 3);
-
+$f3->config("config.ini");
+// Database connection information
+$f3->config("access.ini");
 // Routes
 // index.html
 $f3->route("GET @home: /", "LoginController->render");
 $f3->route("POST @home: /", "LoginController->login");
 $f3->route("GET @register: /register", "RegisterController->render");
 $f3->route("POST @register: /register", "RegisterController->register");
-
 // contact-us.html
 $f3->route("GET @contactUs: /contact-us", "PagesController->contactUs");
 // contact-us-guest.html
 $f3->route("GET @contactUsGuest: /contact-us-guest", "PagesController->contactUsGuest");
-
+ 
 // app.html
 $f3->route("GET @app: /app", "PagesController->app");
 $f3->route("GET @logout: /logout", "PagesController->logout");
-// main.html
-$f3->route("GET @main: /app", "PagesController->app");
+ 
 // Start
 $f3->run();
