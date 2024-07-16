7e4759f67d6c8697f45ad131b16e2046ff10276a
qwqtw <qiutianw@gmail.com>
2024-07-16 12:49:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 // Load the composer required libraries
 require "vendor/autoload.php";
+
 // Load the framework
 $f3 = Base::instance();
 $f3->config("config.ini");
+
 // Database connection information
 $f3->config("access.ini");
+
 // Routes
 // index.html
 $f3->route("GET @home: /", "LoginController->render");
 $f3->route("POST @home: /", "LoginController->login");
 $f3->route("GET @register: /register", "RegisterController->render");
 $f3->route("POST @register: /register", "RegisterController->register");
+
 // contact-us.html
 $f3->route("GET @contactUs: /contact-us", "PagesController->contactUs");
 // contact-us-guest.html
@@ -21,5 +25,6 @@ $f3->route("GET @contactUsGuest: /contact-us-guest", "PagesController->contactUs
 $f3->route("GET @app: /app", "PagesController->app");
 $f3->route("GET @logout: /logout", "PagesController->logout");
  
+
 // Start
 $f3->run();
